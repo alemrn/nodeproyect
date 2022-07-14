29939a8c21ff4554b0f945b747a273091a785671
unknown <amorenoechevarria@gmail.com>
2022-07-14 18:09:18 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 1337;
+
+
+app.listen(port, () => {
+    console.log (`listening at http://localhost:1337`)
+})
