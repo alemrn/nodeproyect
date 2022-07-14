3763bd41f7d8f6ffb15777c9a0755f007a4802c5
unknown <amorenoechevarria@gmail.com>
2022-07-14 19:12:00 +0200

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
 
 const port = process.env.PORT || 1337;
 
-
 app.listen(port, () => {
-    console.log (`listening at http://localhost:1337`)
-})
+  console.log(`listening at http://localhost:${port}`);
+});
