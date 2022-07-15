37ad06f2aa3ecc77ae0acca17cf5ec2fc30c8bc7
unknown <amorenoechevarria@gmail.com>
2022-07-15 10:58:02 +0200

    middleware refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
+const { errorFound, errorStatus } = require('./middleware');
 
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.use(errorFound);
+
+app.use(errorStatus);
 
 const port = process.env.PORT || 1337;
 
